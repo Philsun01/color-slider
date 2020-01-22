3e6b036c31c836b4696a6f92687ec2f35ff2c5ba
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-01-21 19:38:00 -0800

    sizing changes

--- src/App.js
+++ src/App.js
@@ -13,27 +13,24 @@ function App() {
     <div className= "">
       <form>
         <div className = 'slider'>
-          <label>Red</label>
           <input type = 'range' value = {red}
                   min = '0' max = '255' step = '1' 
                   onChange = { (ev) => { setRed(ev.target.value)} }/>
-          <div>{ red }</div>
+          <label>Red {red}</label>
         </div>
 
         <div className = 'slider'>
-          <label>Green</label>
           <input type = 'range' value = {green}
                   min = '0' max = '255' step = '1' 
                   onChange = { (ev) => { setGreen(ev.target.value)} }/>
-          <div>{ green }</div>
+          <label>Green {green}</label>
         </div>
 
         <div className = 'slider'>
-          <label>Blue</label>
           <input type = 'range' value = {blue}
                   min = '0' max = '255' step = '1' 
                   onChange = { (ev) => { setBlue(ev.target.value)} }/>
-          <div>{ blue }</div>
+          <label>Blue {blue}</label>
         </div>
 
       </form>
