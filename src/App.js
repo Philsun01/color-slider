20c75ba537c58fc1783db4634dd4b9e9065ebd97
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-01-21 19:34:12 -0800

    color sliders working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [red, setRed] = useState('125');
+  const [green, setGreen] = useState('125');
+  const [blue, setBlue] = useState('125');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "">
+      <form>
+        <div className = 'slider'>
+          <label>Red</label>
+          <input type = 'range' value = {red}
+                  min = '0' max = '255' step = '1' 
+                  onChange = { (ev) => { setRed(ev.target.value)} }/>
+          <div>{ red }</div>
+        </div>
+
+        <div className = 'slider'>
+          <label>Green</label>
+          <input type = 'range' value = {green}
+                  min = '0' max = '255' step = '1' 
+                  onChange = { (ev) => { setGreen(ev.target.value)} }/>
+          <div>{ green }</div>
+        </div>
+
+        <div className = 'slider'>
+          <label>Blue</label>
+          <input type = 'range' value = {blue}
+                  min = '0' max = '255' step = '1' 
+                  onChange = { (ev) => { setBlue(ev.target.value)} }/>
+          <div>{ blue }</div>
+        </div>
+
+      </form>
+      <div className = 'colorSwatch'
+        style = { 
+            { backgroundColor: `rgb(${red},${green},${blue})`}
+        }>
+
+      </div>
+
+
     </div>
   );
 }
